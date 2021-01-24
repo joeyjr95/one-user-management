72160119d443e3726105c10c0fe536972fdba7e5
joeyjr95 <joeyjr95@gmail.com>
2021-01-24 17:59:49 -0500

    added mongoose

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const mongoose = require("mongoose");
 
 
 const app = express();
@@ -17,7 +18,7 @@ app.use(helmet());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!');
+  res.send('Hello, One!');
 });
 
 app.use(function errorHandler(error, req, res, next){
